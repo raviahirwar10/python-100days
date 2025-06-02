36fc6612ae5e44cb03666f2a9dd553bb44fea523
raviahirwar10 <ravi9993333@gmail.com>
2025-06-02 18:47:37 +0530

    day 3

---
+++ day 3 .py
@@ -0,0 +1,20 @@
+# hay don't remove this line bro 
+# auther: ravi ahirwar
+# day 3 : and video 5
+print("hey i am a boy\nbut you tell me whether i am god boy or abd boy")
+'''
+hay don't remove this line bro 
+auther: ravi ahirwar
+day 3 : and video 5
+'''
+# Escape Sequence Characters
+print("hey \"i am a boy\" \nand \'you\' ")
+# More on Print statement
+print("hey",5+5,sep="-",end="0001\n")
+print("ravi ahirwar")
+# tis is if else statement 
+p = 7
+if (p>5):
+    print("p is greater then 5.")
+else:
+    print("p is not greater then 5.")
