fb70803cb25d22e8ec0b0792fbdd2bba71db0ae2
raviahirwar10 <ravi9993333@gmail.com>
2025-06-05 13:38:40 +0530

    2.0

---
+++ day make2.0 .py
@@ -0,0 +1,5 @@
+age=18
+if age>=18:
+ print("if you are eligible for vote")
+else:
+    print("if you aare not 18 so you are not eligible for vote")
---
+++ program month name .py
@@ -0,0 +1,26 @@
+month = 1
+if  month==1:
+    print('jan')
+elif month==2:
+    print('feb')
+elif month==3:
+    print('mar')
+elif month==4:
+    print('apr')
+elif month==5:
+    print('may')
+elif month==6:
+    print('jun')    
+elif month==7:
+    print('july')
+elif month==8:
+    print('aug')
+elif month==9:
+    print('sep')
+elif month==10:
+    print('oct')
+elif month==11:
+    print('nov')
+elif month==12:
+    print('dec')
+    
