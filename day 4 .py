6c0e6c972a11d20083181c4aafaad7d854ad9011
raviahirwar10 <ravi9993333@gmail.com>
2025-06-05 20:41:45 +0530

    variables

---
+++ day 4 .py
@@ -0,0 +1,22 @@
+# Variables and Data Types
+a = 12233321
+print (a)
+B = "ravi ahirwar "
+print (B)
+C = complex(9,10)
+print (C)
+D = True
+print (D)
+E = 45.7
+print (E)
+print ("the type of a is", type(a))
+print ("the type of B is", type(B))
+print ("the type of C is", type(C))
+print ("the type of D is", type(D))
+print ("this type of E", type(E))
+# modified after creation
+list = [34 , 2.3 , [-45,4] , ["ravi","ahirwar"]]
+print (list)
+# immutable and can not be modified after creation.
+tuple = (("LION","TIGER"))
+print (tuple)
