94e2f0d8094f7972612a328060fc696401d60328
raviahirwar10 <ravi9993333@gmail.com>
2025-06-10 13:58:10 +0530

    HELOO

--- day 5.py
+++ day 5.py
@@ -1,21 +1,7 @@
 # projrct calculater
-num1 = float(input("Phale sankhya dala"))
-num2 = float(input("dusri sankhya dala"))
-opration = input("in ma sa koi ek opration chuna? (+,-,*,/)")
-
-if opration == "+":
-    result = num1 + num2
-    print (f"result: {num1}+{num2} = {result}")
-    
-elif opration == "-":
-    result = num1 - num2
-    print(f"result: {num1} - {num2} = {result}")
-elif opration == "*":
-    result = num1 * num2
-    print(f"result: {num1} * {num2} = {result}")
-    
-elif opration == "/":
-    result = num1 / num2
-    print(f"result: {num1} / {num2} = {result}")
-    
-    
+A = 5
+B = 60
+print ( "the value of", A, "+", B,"is :",A + B )
+print ( "the value of", A, "*", B,"is :",A * B )
+print ( "the value of", A, "-", B,"is :",A - B )
+print ( "the value of", A, "/", B,"is :",A / B )
