d1381938713c05f65d94b90b0aef96c3bc88609e
raviahirwar10 <ravi9993333@gmail.com>
2025-06-12 17:27:04 +0530

    typecasting

---
+++ day 6.py
@@ -0,0 +1,15 @@
+# learn typecastinig
+# this is a string value
+a = "10"
+b = "2"   
+print (a+b) 
+# canvert to string to int 
+# 1. explicit conversion
+c = "9"
+d = "9"
+print (int(c)+int(d))
+
+# 2. implicit conversion
+e = 3.7
+f = 4
+print (e+f)
