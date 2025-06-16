aa6de8e61a9b9a082f0469241a38a8bc6a934c60
raviahirwar10 <ravi9993333@gmail.com>
2025-06-16 11:07:46 +0530

    projerct

---
+++ input projec.py
@@ -0,0 +1,14 @@
+num = int(input("enter a num:"))
+print(num,"",type(num))
+
+float = float(input("ent a num:"))
+print (float,"",type(float))
+
+noo = input("ente a num:")
+print(noo)
+
+name = input("enter a name:")
+input (name)
+
+print("type of num",type(noo))
+print("type of name",type(name))
