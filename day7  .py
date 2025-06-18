40c68965fce710ecbfcbf511b88f4be8aec5661b
raviahirwar10 <ravi9993333@gmail.com>
2025-06-18 13:08:01 +0530

    input

--- day7  .py
+++ day7  .py
@@ -1,6 +1,7 @@
 # day 7 input
 a = input("what is your name = ")
 print ("my name is", a)
+print("what type is",type(a))
 
 c = input("Enter a first number =")
 d = input("Enter a second  number =")
