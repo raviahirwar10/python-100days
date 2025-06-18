c73afedf3fe802ed17890168487f60097bc335ae
raviahirwar10 <ravi9993333@gmail.com>
2025-06-18 12:54:57 +0530

    loop

--- day 8.py
+++ day 8.py
@@ -1,9 +1,17 @@
 # topic strings
-namae = "ravi"
-print ("hey,"+namae)
+name = "ravi"
+print ("hey,"+name)
 
 sid = '''He said, 
 Hi Harry
 "hey I am good"
 "I want to eat an apple'''
 print(sid)
+
+print (name[0])
+print (name[2])
+print (name[3])
+# print (name[4]) = throws a error 
+print("let use a for loop\n")
+for charcter in sid:
+    print(charcter)
