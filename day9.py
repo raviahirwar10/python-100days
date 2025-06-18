eaa129a972400732579cf00f45f40d6c29ce74f2
raviahirwar10 <ravi9993333@gmail.com>
2025-06-18 13:34:49 +0530

    slice

---
+++ day9.py
@@ -0,0 +1,18 @@
+# string slicing
+fruit = "mango"
+fruitlen = len(fruit)
+# looping
+print (fruit[0]) 
+print (fruit[2]) 
+print (fruit[3]) 
+print (fruit[4]) 
+
+# slice len
+print(fruitlen)
+print(fruit[2:4])
+print(fruit[0:4])
+print(fruit[-3:-5])
+print(fruit[-3:4])
+
+nm = "harry"
+print (nm[-4:-2])
