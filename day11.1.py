f210e009eaeb1fe3e25905cacab281b159681521
raviahirwar10 <ravi9993333@gmail.com>
2025-06-20 16:40:01 +0530

    num if else elif

---
+++ day11.1.py
@@ -0,0 +1,11 @@
+num = int(input("enter a number :"))
+if (num < 0):
+    print("nnumber is nagativ")
+elif (num == 0):
+    print("number is zero")
+elif (num == 555):
+    print("number is spacel")
+else :
+    print("number is positive")
+    
+print(" hey i'm happy now ")
