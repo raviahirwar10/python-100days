769615920d03f8eb8d4b480cc4c51ce57f453957
raviahirwar <ravi9993333@gmail.com>
2025-06-27 16:39:02 +0530

    while loop

---
+++ day14.py
@@ -0,0 +1,22 @@
+# while loop
+# increment loop
+
+num = int(input("enter a number"))
+while (num<90):
+    num = int(input("enter a number"))
+    print(num)
+print("bass itna hi tha ")
+print("i am done")
+
+# decrement loop
+count = 10
+while(count>0):
+    print(count)
+    count=count-1
+    
+# infinite nnum loop
+number=10
+while(number>0):
+    print(number)
+    number=number+1
+    
