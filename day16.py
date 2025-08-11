91dcd1400e7ba911a5a65228449e888920dd2a06
raviahirwar <ravi9993333@gmail.com>
2025-08-11 16:44:14 +0530

    functon

--- day16.py
+++ day16.py
@@ -3,7 +3,7 @@ def gcount (a,b):
     count = (a*b)/(a+b)
     print(count)
     
-def ggrater (a,b):
+def grater (a,b):
    if (a>b):
     print("first num is greater ")
    else:
@@ -15,10 +15,10 @@ def ggrater (a,b):
 a = 20
 b = 52
 gcount(a,b)
-ggrater(a,b)
+grater(a,b)
 
 
 a1 = 88
 b1 = 87
 gcount(a1,b1)
-ggrater(a1,b1)
+grater(a1,b1)
