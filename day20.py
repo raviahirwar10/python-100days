40f021e25796690c9983cc083f2227bd88cf76e6
raviahirwar <ravi9993333@gmail.com>
2025-08-28 19:16:21 +0530

    fsting

---
+++ day20.py
@@ -0,0 +1,10 @@
+# topic = f-string
+latter = "hey my name {} and i am from {}"
+name = "krishnkant"
+country = "india"
+# print(latter.format(name,country)) #string formating 
+
+print(f"hey my name {country } and i am from {name}")
+print(f"hey my name {{country }} and i am from {{name}}")
+
+
