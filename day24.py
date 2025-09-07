768dec2a498765ae6f2344ceb5ea57b94d72b4cb
raviahirwar <ravi9993333@gmail.com>
2025-09-07 18:22:05 +0530

    dictionaries

---
+++ day24.py
@@ -0,0 +1,10 @@
+#dictionaries 
+
+dict1 = {'name':"anju", 'age': 25, 'city': 'New York'}
+print(dict1["age"])
+print(dict1.keys())
+print(dict1.values())
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info['name'])
+print(info.get('eligible')) #if key not present it will return none
