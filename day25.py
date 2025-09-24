5e968bcb761c601d8c2fd81f2afa556e64072ee9
raviahirwar <ravi9993333@gmail.com>
2025-09-24 18:50:04 +0530

    dictionary method

---
+++ day25.py
@@ -0,0 +1,17 @@
+#dictionary method
+
+air1 = {22:34,45:22,23:44,454:33,99:33}
+air2 = {222: 67, 566: 90}
+
+#air1.update(air2)
+#air2.clear()
+#print(air2)
+
+#air1.pop(99)
+#print(air1)
+
+air2.popitem()
+print(air2)
+
+del air1[45]
+print(air1)
