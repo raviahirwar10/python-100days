c4d53331359480d79adac6d11a41c64d9a59fcd6
raviahirwar <ravi9993333@gmail.com>
2025-10-03 17:46:40 +0530

    else - lopp

---
+++ day26.py
@@ -0,0 +1,9 @@
+#if-else in a for loop.
+a=0
+for i in range(9):   
+    if i == 8:
+        break
+    print(i)
+
+else:
+    print("sorry i ") # if yu use break, the values after the brak will not be
