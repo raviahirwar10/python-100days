1eedc208911393dbe20e18b6520dfd8c0fa70683
raviahirwar <ravi9993333@gmail.com>
2025-12-14 20:07:23 +0530

    class and objects

---
+++ day43.py
@@ -0,0 +1,25 @@
+# class and objects
+
+class persion:
+    name = "ravi"
+    age = "19"
+    occupation = "data analyst"
+    def desc(self):
+        print("my name is",self.name,"and i m",self.age,"years old","and im a",self.occupation)
+
+a = persion()
+b = persion()
+age1 = int(input("enter your age :"))
+a.age=(age1)
+name1=input("Enter your name :")
+a.name=(name1)
+occ1=input("Enter your occupation :")
+a.occupation=(occ1)
+b.desc()
+a.desc()
+# obj1=persion()
+# obj1.desc()
+    
+
+    
+ 
