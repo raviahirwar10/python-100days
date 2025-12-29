e2f08152e3408d498b74d4c27bc492522b769578
raviahirwar <ravi9993333@gmail.com>
2025-12-29 12:40:43 +0530

    decorators

---
+++ day45.py
@@ -0,0 +1,17 @@
+#topic dacorators.
+def greet(fx):
+ def mfx(*args,**kwargs):
+    print("hello world")
+    fx(*args,**kwargs)
+    print("thankyou for using this programe ")
+ return mfx
+# @greet
+# def hello():
+#     print("heloo world")
+    
+@greet
+def add(a,b):
+    print(a+b)
+    
+# hello()
+add(9,4)
